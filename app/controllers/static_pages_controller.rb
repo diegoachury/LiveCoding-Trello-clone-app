0fb4a9f554ca178f8ae233a28f513858382e5a33
Toni Češarek <programmertoni@gmail.com>
2015-08-12 06:27:02 +0200

    Adds navigation jumbotron custom colorscheme and gitignore file

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,9 @@
 class StaticPagesController < ApplicationController
 
+  def about_us
+  end
+
+  def help_center
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/about-us',    to: 'static_pages#about_us'
   get '/help-center', to: 'static_pages#help_center'
+
+  resources :boards
 end
