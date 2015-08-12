8edc70c38c2bb198c75f8d77879317e360b71b63
Toni Češarek <programmertoni@gmail.com>
2015-08-12 12:33:39 +0200

    adds sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,18 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   get '/about-us',    to: 'static_pages#about_us'
   get '/help-center', to: 'static_pages#help_center'
 
+  get    '/login',        to: 'sessions#new'
+  post   '/login',        to: 'sessions#create'
+  delete '/logout',       to: 'sessions#destroy'
+
+  get    '/sign-up',       to: 'users#new'
+
+  resources :users, only: [:create, :update, :edit, :destroy]
   resources :boards
 end
