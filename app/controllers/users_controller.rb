6b9312af9b433aef392937c0bf616a81a0fd5568
Toni Češarek <programmertoni@gmail.com>
2015-08-13 11:43:42 +0200

    Adds login page and turbolinks loader

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to boards_path
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -14,5 +21,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_many :boards, dependent: :destroy
-
   has_secure_password
+
+  validates :name, presence: true,
+                   length: { minimum: 3, maximum: 20 },
+                   uniqueness: { case_sensitive: false }
 end
