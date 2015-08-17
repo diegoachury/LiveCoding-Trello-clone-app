2fcfa635987a411f6c1d0b167fae0307c99a4f4d
Toni Češarek <programmertoni@gmail.com>
2015-08-18 00:34:42 +0200

    adds changing text buttons to the cards form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,26 @@ Turbolinks.enableProgressBar();
 // $('.board').hover(function(){
 //   $('.board-buttons').show();
 // })
+
+function listButton(){
+  $('.list-box').on('click', '.card-form-button', function(){
+    var form = $(this).closest('.list-box').children('.list-form');
+
+      form.slideToggle()
+    if(form.hasClass('hidden-list-form')){
+      form.removeClass('hidden-list-form')
+      $(this).text('Hide this form')
+             .removeClass('hidden-list-form')
+             .addClass('big-opacity')
+        
+    } else {
+      form.addClass('hidden-list-form')
+      $(this).text('Add Card')
+             .addClass('hidden-list-form')
+             .removeClass('big-opacity ')
+    }
+  })
+}
+
+$(document).ready(listButton)
+$(document).on('page:load', listButton)
