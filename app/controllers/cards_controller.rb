359e99ac82c5818cd79fa211dc8126fadb7d7d6f
Toni Češarek <programmertoni@gmail.com>
2015-08-19 12:47:28 +0200

    adds ajax delete and editing capabileties to the app

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,16 +1,48 @@
 class CardsController < ApplicationController
+  before_action :get_user_board_list, only: [:create, :edit, :update, :destroy]
   def create
-    @list = List.find(params[:list_id])
     @card = @list.cards.build(name:        params[:name], 
                               description: params[:description],
                               position:    params[:position]) 
     
-
     if @card.save
       respond_to do |format|
         format.js
       end
     else
     end
+  end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    @card.update(params.require(:card).permit(:name, :description, :position))
+
+    if @card.save
+      flash[:success] = "Card: '#{@card.name}' was updated."
+      redirect_to user_board_path(@user, @board)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def get_user_board_list
+    @user  = User.find(params[:user_id])
+    @board = Board.find(params[:board_id])
+    @list  = List.find(params[:list_id])
   end
 end
